336c67cbc0c469cf85cf3d18f303a8b7e7ffba6b
Andrew Yurovchak <andy@yurovchak.net>
2024-06-02 20:44:34 -0400

    mascot change

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use qart::qr::Code;
 
 fn main() {
     let code = Code::new(String::from("https://github.com/andrewyur/qart"), 40);
-    let img_res = code.build(2, 30, String::from("target.jpg"), 100);
+    let img_res = code.build(5, 60, String::from("target.jpg"), 100);
     match img_res {
-        Ok(img) => img.save("code.png").unwrap(),
+        Ok(img) => img.save("mascot.png").unwrap(),
         Err(s) => println!("{}", s),
     }
 
