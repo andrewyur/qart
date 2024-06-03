4bc7ed47eff06058da21d47def7b6f5cf8019c49
Andrew Yurovchak <andy@yurovchak.net>
2024-06-02 20:34:06 -0400

    edit mascot size

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use qart::qr::Code;
 
 fn main() {
     let code = Code::new(String::from("https://github.com/andrewyur/qart"), 40);
-    let img_res = code.build(5, 60, String::from("horse.png"), 100);
+    let img_res = code.build(1, 12, String::from("mascot 1.jpg"), 100);
     match img_res {
         Ok(img) => img.save("code.png").unwrap(),
         Err(s) => println!("{}", s),
